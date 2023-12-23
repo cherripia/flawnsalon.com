e70fbd4d2d4df2074ff0c55eefce4a72327fd7e9
cherri <cherririri@protonmail.ch>
2023-12-23 09:31:56 +0000

    Tabs lol

--- src/index.js
+++ src/index.js
@@ -9,31 +9,31 @@ app.use(helmet());
 const port = process.env.PORT || 3000;
 const groupId = process.env.GROUP_ID;
 
-async function login() {
-    try {
-        const currentUser = await noblox.setCookie(process.env.ROBLOSECURITY);
-        console.log(`Logged in as ${currentUser.UserName} [${currentUser.UserID}]`);
-    } catch (err) {
-        console.log("Unable to log in!", err);
-    }
+async function robloxLogin() {
+	try {
+		const currentUser = await noblox.setCookie(process.env.ROBLOSECURITY);
+		console.log(`Logged in as ${currentUser.UserName} [${currentUser.UserID}]`);
+	} catch (err) {
+		console.log("Unable to log in!", err);
+	}
 }
 
-login();
+robloxLogin();
 
 app.get('/api', async function(req, res) {
-    try {
-        const currentUser = await noblox.getCurrentUser();
-        console.log(`Current user: ${currentUser.UserName} [${currentUser.UserID}]`);
-    } catch (err) {
-        console.log("Unable to log in!", err);
-    }
-    res.send('api');
+	try {
+		const currentUser = await noblox.getCurrentUser();
+		console.log(`Current user: ${currentUser.UserName} [${currentUser.UserID}]`);
+	} catch (err) {
+		console.log("Unable to log in!", err);
+	}
+	res.send('api');
 });
 
 app.get('/', function (req, res) { 
-    res.redirect('https://www.roblox.com/groups/33267151/Flawn-Salon');
+	res.redirect('https://www.roblox.com/groups/33267151/Flawn-Salon');
 });
 
 app.listen(port, function () { 
-    console.log(`Server running on port ${port}`);
+	console.log(`Server running on port ${port}`);
 });
