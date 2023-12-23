c2d88aeb79260ed7731fa1a5cb31d0ab6311c26b
cherri <cherririri@protonmail.ch>
2023-12-23 08:40:52 +0000

    Added noblox login

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-const express = require('express'); 
+const express = require('express');
+const noblox = require('noblox.js');
+const helmet = require('helmet');
+require('dotenv').config();
 
 const app = express();
+app.use(helmet());
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+const groupId = process.env.GROUP_ID;
 
-app.get('/api', function(req, res) {
+async function login() {
+    try {
+        const currentUser = await noblox.setCookie(process.env.ROBLOSECURITY);
+        console.log(`Logged in as ${currentUser.UserName} [${currentUser.UserID}]`);
+    } catch (err) {
+        console.log("Unable to log in!", err);
+    }
+}
+
+login();
+
+app.get('/api', async function(req, res) {
+    try {
+        const currentUser = await noblox.getCurrentUser();
+        console.log(`Current user: ${currentUser.UserName} [${currentUser.UserID}]`);
+    } catch (err) {
+        console.log("Unable to log in!", err);
+    }
     res.send('api');
-})
+});
 
 app.get('/', function (req, res) { 
-    res.send('Hello, world!');
-})
+    res.redirect('https://www.roblox.com/groups/33267151/Flawn-Salon');
+});
 
-app.listen(3000, function () { 
+app.listen(port, function () { 
     console.log(`Server running on port ${port}`);
-})
+});
