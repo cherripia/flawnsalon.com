d341e97faa9c6b79b7e8993ad3470dec073636f0
cherri <cherririri@protonmail.ch>
2023-12-23 00:57:11 -0500

    Basic web server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express'); 
 
-const app = express(); 
+const app = express();
+
+const port = 3000;
+
+app.get('/api', function(req, res) {
+    res.send('api');
+})
 
 app.get('/', function (req, res) { 
-    res.send("Hello World!, I am server created by expresss"); 
+    res.send('Hello, world!');
 })
 
 app.listen(3000, function () { 
-    console.log("server started"); 
+    console.log(`Server running on port ${port}`);
 })
